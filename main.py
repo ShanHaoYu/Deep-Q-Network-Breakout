4a5519b36277b53e1c5d81e15d432d8b7adde09a
于善豪(Chris) <ShanHaoYu@users.noreply.github.com>
2018-01-29 19:59:54 +0800

    Some basic code

---
+++ main.py
@@ -0,0 +1,39 @@
+import argparse
+from test import test
+from environment import Environment
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="MLDS&ADL HW3")
+    parser.add_argument('--env_name', default=None, help='environment name')
+    parser.add_argument('--train_dqn', action='store_true', help='whether train DQN')
+    parser.add_argument('--test_dqn', action='store_true', help='whether test DQN')
+    parser.add_argument('--video_dir', default=None, help='output video directory')
+    parser.add_argument('--do_render', action='store_true', help='whether render environment')
+    try:
+        from argument import add_arguments
+        parser = add_arguments(parser)
+    except:
+        pass
+    args = parser.parse_args()
+    return args
+
+
+def run(args):
+    # All frames are preprocessed with atari wrapper.
+    if args.train_dqn:
+        env_name = args.env_name or 'BreakoutNoFrameskip-v4'
+        env = Environment(env_name, args, atari_wrapper=True)
+        from agent_dir.agent_dqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        agent.train()
+
+    if args.test_dqn:
+        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+        from agent_dir.agent_dqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        test(agent, env, total_episodes=100)
+
+if __name__ == '__main__':
+    args = parse()
+    run(args)
---
+++ test.py
@@ -0,0 +1,51 @@
+import argparse
+import numpy as np
+from environment import Environment
+
+seed = 11037
+
+def parse():
+    parser = argparse.ArgumentParser(description="DQN")
+    parser.add_argument('--test_dqn', action='store_true', help='whether test DQN')
+    parser.add_argument('--video_dir', default=None, help='output video directory')
+    parser.add_argument('--do_render', action='store_true', help='whether render environment')
+    try:
+        from argument import add_arguments
+        parser = add_arguments(parser)
+    except:
+        pass
+    args = parser.parse_args()
+    return args
+
+
+def test(agent, env, total_episodes=30):
+    rewards = []
+    env.seed(seed)
+    for i in range(total_episodes):
+        state = env.reset()
+        agent.init_game_setting()
+        done = False
+        episode_reward = 0.0
+
+        #playing one game
+        while(not done):
+            action = agent.make_action(state, test=True)
+            state, reward, done, info = env.step(action)
+            episode_reward += reward
+
+        rewards.append(episode_reward)
+    print('Run %d episodes'%(total_episodes))
+    print('Mean:', np.mean(rewards))
+
+
+def run(args):
+    if args.test_dqn:
+        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+        from agent_dir.agent_dqn import Agent_DQN
+        agent = Agent_DQN(env, args)
+        test(agent, env, total_episodes=100)
+
+
+if __name__ == '__main__':
+    args = parse()
+    run(args)
